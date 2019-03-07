73e35c2f32bafa4288f68930bc7d4cda7938cd73
Hans Goor <rooteddev@outlook.com>
2019-03-07 16:49:43 +0100

    Added a new breeding function

--- network/util/breeding/__init__.py
+++ network/util/breeding/__init__.py
@@ -1,7 +1,9 @@
+from network.util.breeding import crossover_half
 from .crossover import crossover
 from .random_gene_copy import random_gene_copy
 
 breeding_functions = {
     "crossover": crossover,
+    "crossover_half": crossover_half,
     "random_gene_copy": random_gene_copy,
 }
