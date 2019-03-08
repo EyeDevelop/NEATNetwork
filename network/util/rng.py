6d4bbb0e81cf1aa8df95f37097fff1d0533e826c
Hans Goor <rooteddev@outlook.com>
2019-03-08 22:12:12 +0100

    Added a Gaussian mean random function

--- network/util/rng.py
+++ network/util/rng.py
@@ -8,6 +8,11 @@ def random_check():
     random.seed()
 
 
+def random_gaussian(mean=0, std_deviation=1):
+    random_check()
+    return random.gauss(mean, std_deviation)
+
+
 def randint(a, b):
     random_check()
     return random.randint(a, b)
